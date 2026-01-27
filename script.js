ec734c585bd69a8357044cae42a06c4dbd4b5172
philward_io <54246354+phil-ward@users.noreply.github.com>
2026-01-27 10:00:01 +0100

    Add Human Choice Function
    Create getHumanChoice function to prompt user to enter their choice and
    return

--- script.js
+++ script.js
@@ -12,4 +12,23 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    // Function that takes the human player choice and returns the value
+    let humanChoice = parseInt(prompt(`What would you like to play?\n
+         1. Rock\n
+         2. Paper\n
+         3. Scissors\n
+         Please enter the number of your choice:`))
+
+    switch (humanChoice) {
+        case 1:
+            return 'rock'
+        case 2: 
+            return 'paper'
+        case 3:
+            return 'scissors'
+    }
+
+}
+
+console.log(getHumanChoice())
